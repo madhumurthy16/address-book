d661b337962bdc284c93a5f80482839ed7c95043
Madhu Murthy <msmurthy@msn.com>
2019-01-11 17:36:54 -0800

    Listing contacts and searching completed

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,4 +1,3 @@
-const inquirer = require("inquirer");
 const Contact = require("../db/models").Contact;
 
 module.exports = class ContactController {
@@ -15,7 +14,7 @@ module.exports = class ContactController {
 			},
 			{
 				type: "input",
-				name: "name",
+				name: "phone",
 				message: "Contact's phone number - ",
 				validate(val) {
 					return val !== "";
@@ -23,16 +22,110 @@ module.exports = class ContactController {
 			},
 			{
 				type: "input",
-				name: "name",
+				name: "email",
 				message: "Contact's email - ",
 				validate(val) {
 					return val !== "";
 				}
 			}
 		];
+
+		this.searchQuestions = [
+			{
+				type: "input",
+				name: "name",
+				message: "Name of contact to search - ",
+				validate(val) {
+					return val !== "";
+				}
+			}
+		];
+
+		this.showContactQuestions = [
+			{
+				type: "list",
+				name: "selected",
+				message: "Please choose from an option below: ",
+				choices: [
+					"Delete contact",
+					"Main menu"
+				]
+			}
+		];
+
+		this.deleteConfirmQuestions = [
+			{
+				type: "confirm",
+				name: "confirmation",
+				message: "are you sure you want to delete this contact?"
+			}
+		];
 	}
 
 	addContact(name, phone, email) {
-		return Contact.create({name, phone, email});
+		return Contact.create({name, phone, email})
 	}
+
+	getContacts() {
+		return Contact.findAll();
+	}
+
+	iterativeSearch(contacts, target){
+		for(let contact of contacts) {
+			if(contact.name.toLowerCase() === target.toLowerCase()) {
+				return contact;
+			}
+		}
+		return null;
+	}
+
+	binarySearch(contacts, target) {
+		let min = 0;
+		let max = contacts.length - 1;
+		let mid;
+		while(min <= max) {
+			mid = Math.floor(min+max)/2;
+			let currentContact = contacts[mid];
+			if(currentContact.name > target) {
+				max = mid-1;
+			}
+			else if(currentContact.name < target) {
+				min = mid - 1;
+			}
+			else {
+				return contacts[mid];
+			}
+		}
+		return null;
+	}
+
+	search(name) {
+		return Contact.findOne({
+			where: {name}
+		});
+	}
+
+	delete(id) {
+		return Contact.destroy({
+			where: {id}
+		})
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
